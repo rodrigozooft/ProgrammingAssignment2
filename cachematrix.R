4d406a8b968b85d67dddabfff535251bb63e20aa
Rod Morgado <rodrigo@zooft.company>
2021-07-25 15:54:24 -0400

    Creating the function to set the value of the matrix given by the user

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set_matrix_value <- function(matrix){
+    x <<- matrix
+    m <<- NULL
+  }
 }
 
 
